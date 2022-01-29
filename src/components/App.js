030aac297932ea7c850fc8048202d344880a23a3
dmonyo <devel.enterprise@gmail.com>
2022-01-29 15:58:40 -0500

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+
+
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <Navbar />
+      <h1 className="text-3xl text-blue-400 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
